048202a34a9bc7a019be9514ef9672b3a5c1a88e
Dhruv Balchandani <119007749+dhruvx19@users.noreply.github.com>
2024-01-24 01:19:13 +0530

    dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_2/assets/dashboard.dart';
 
 void main() => runApp(const MyApp());
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
