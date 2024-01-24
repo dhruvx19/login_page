cce8f1d51e61f54607ca384358e1f7733a998422
Dhruv Balchandani <119007749+dhruvx19@users.noreply.github.com>
2024-01-24 23:33:11 +0530

    container

--- lib/assets/dashboard.dart
+++ lib/assets/dashboard.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
@@ -6,28 +9,43 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'.toUpperCase()),
-        backgroundColor: Colors.deepPurple,
-      ),
-      body: const Center(
-        child: Text.rich(
-          TextSpan(
-            text: 'My',
-            style: TextStyle(fontSize: 30),
-            children: [
-              TextSpan(
-                text: 'Flutter',
-                style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-              ),
-              TextSpan(
-                text: 'App',
-                style: TextStyle(fontSize: 40, color: Colors.blue),
-              ),
-            ],
-          ),
+      backgroundColor: Colors.black,
+      body: Container(
+        width: 300,
+        height: 300,
+        // color: Colors.grey,
+        padding: const EdgeInsets.all(50),
+        margin: const EdgeInsets.all(50),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          image: const DecorationImage(image: AssetImage("images/b.jpg")),
+          shape: BoxShape.circle,
+          //borderRadius: BorderRadius.circular(50),
+          color: Colors.grey,
+          border: Border.all(color: Colors.white, width: 6.0),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.white,
+              blurRadius: 7,
+              spreadRadius: 5,
+              offset: Offset(4, 4),
+            )
+          ],
         ),
+        // child: const Text(
+        //   '',
+        //   style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        // ),
       ),
     );
   }
 }
+
+int getNumber() {
+  return Random().nextInt(100);
+}
+// Random random = new Random();
+// var number = random.nextInt(10);
+// return number;
+
+void press() {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/assets/dashboard.dart';
 
-void main() => runApp(const MyApp());
+void main() =>runApp(const MyApp());
+
+
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  const MaterialApp(
       title: 'Flutter',
-      themeMode: ThemeMode.system,
       home: Dashboard(),
     );
   }
